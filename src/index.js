3e428f6f84d9fff50d39f2757d08035ee0ded62f
Smit Kalavadiya <smitkalavadiya99@gmail.com>
2024-07-24 19:21:00 -0300

    databse connection added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 
 databaseConnection();
 
-app.post('/register', function (req, res) {
-    res.send(req.body);
+app.get('/', function (req, res) {
+    res.send("Hello World");
   })
   
 app.listen(process.env.PORT || 3000);
