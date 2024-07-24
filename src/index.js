faed5f18b5a7b189b4eed4bfe5a149657724287a
smit kalavadiya <58399424+smit-kalavadiya@users.noreply.github.com>
2024-07-24 14:58:58 -0300

    express added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+const express = require('express');
 require('dotenv').config();
 const {databaseConnection} = require('./db/database');
 
+const app = express();
 
 databaseConnection();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+  })
+  
+app.listen(process.env.PORT || 3000);
 
